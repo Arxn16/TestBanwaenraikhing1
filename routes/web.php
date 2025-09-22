8c4b47b6ffb997d9aa3a8373ac205f1f06b1ebe2
Thanapong <thanapong24471@gmail.com>
2025-09-22 21:00:51 +0700

    แก้อีก

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,7 @@ Route::get('/', function () {
 
 Route::prefix('admin')->group(function() {
     // Route สำหรับหน้าแสดงสินค้าของแอดมิน
-    Route::get('products', [ProductController::class, 'productsList'])->name('admin.products.index');
-    Route::get('products/create', [ProductController::class, 'create'])->name('admin.products.create');
-    Route::post('products', [ProductController::class, 'store'])->name('admin.products.store');
-    Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
-    Route::put('products/{id}', [ProductController::class, 'update'])->name('admin.products.update');
-    Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
+ 
     // ใน routes/web.php หรือ routes/admin.php
 Route::get('admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
 Route::post('admin/products', [ProductController::class, 'store'])->name('admin.products.store');
